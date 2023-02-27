00978e94dc5a020b700dfc95bce54b7885efb8e7
Emerald-SRG <97896014+Emerald-SRG@users.noreply.github.com>
2023-02-27 19:52:13 +0200

    task 8 DONE

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formField = document.querySelector(".login-form");
 
+const toSubmit = (event) => {
+    event.preventDefault();
+    const {
+        elements: {email, password }
+    } = event.currentTarget;
+
+    if (email.value.length < 1 || password.value < 1) {
+        return alert("All fields must be filled! Check it.");
+    }
+    const userForm = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(userForm);
+
+    event.currentTarget.reset();
+};
+
+formField.addEventListener("submit", toSubmit);
