6c2c158f69429117ff603f86b4836b5ebdeb6dc8
Emerald-SRG <97896014+Emerald-SRG@users.noreply.github.com>
2023-02-27 20:20:58 +0200

    task 9 DONE

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const backGround = document.body;
+const colorStyle = document.querySelector("span.color");
+const changeCol = document.querySelector("button.change-color");
+
+changeCol.addEventListener("click", () => {
+  backGround.style.backgroundColor = getRandomHexColor();
+  changeCol.textContent = backGround.style.backgroundColor;
+});
